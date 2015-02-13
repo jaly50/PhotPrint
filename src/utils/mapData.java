b37c4dd7bb5d9041658b1061683605943f3fd6fd
charlottelinxue <charlottelinxue@gmail.com>
2015-02-12 20:25:44 -0500

    add some util from previous students

---
+++ src/utils/mapData.java
@@ -0,0 +1,28 @@
+package utils;
+
+public class mapData {
+	public double lat;
+	public double lon;
+
+	public mapData(double lat, double lon) {
+		this.lat = lat;
+		this.lon = lon;
+	}
+
+	public double getLat() {
+		return lat;
+	}
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+	public double getLon() {
+		return lon;
+	}
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+
+}
