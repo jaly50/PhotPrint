638bbf1bdd482de65f6c041cc8f7d2ac0ba1702c
jaly50 <jialic@andrew.cmu.edu>
2015-02-16 13:39:13 -0500

    Some changes

--- WebContent/WEB-INF/template-top.jsp
+++ WebContent/WEB-INF/template-top.jsp
@@ -1,6 +1,8 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <head>
+
+<link rel="SHORTCUT ICON" href="images/logo.jpeg"> 
 <meta charset="utf-8" />
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, maximum-scale=1" />
--- src/controller/UploadAction.java
+++ src/controller/UploadAction.java
@@ -123,7 +123,7 @@ public class UploadAction extends Action {
 			// Clean the cached files
 			FileUploadServlet.files = new LinkedList<FileMeta>();
 
-			return "showWrapper.do";
+			return "showTweet.jsp";
 		} catch (RollbackException e) {
 			errors.add(e.getMessage());
 			return "manage.jsp";
